e86db6a543c02b8bf61d95a799acbd0f6a5de92a
joshgrant <joshua.m.grant@gmail.com>
2019-09-03 13:45:25 -0400

    split into two

--- test/specs/functional.spec.js
+++ test/specs/functional.spec.js
@@ -15,23 +15,4 @@ describe('Temperature converter', () => {
 
         assert.equal($(".App-logo").isDisplayed(), true);
     });
-
-    it('should show the correct initial conversion message', () => {
-        browser.url('/');
-
-        const actualMessage = $('.temperatureMesssage').getText();
-        const expectedMessage = "Watiting for input...";
-
-        assert.equal(actualMessage, expectedMessage);
-    });
-
-    it('should show the correct conversion message after conversion', () => {
-        browser.url('/');
-
-        $('.scale-type-c').setValue("0");
-
-        const actualMessage = $('.temperatureMesssage').getText();
-
-        assert.equal(actualMessage.includes("0 Celsius is 32 Farhenheit"), true);
-    });
 });
