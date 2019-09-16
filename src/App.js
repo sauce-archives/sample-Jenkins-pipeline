774d582e086abfbc17377ba31a5630f4aef187fb
joshgrant <joshua.m.grant@gmail.com>
2019-09-16 13:25:09 -0400

    more egregious typo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-  }
+  
 
   handleChange(e) {
     this.props.onTemperatureChange(e.target.value);
